5cb7fdfbfc7dc7caa2e2db046781b77bdc500d29
Joey Gaitan <joeygaitan707@gmail.com>
2019-11-02 17:21:33 -0700

    added in backend

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
 
+const admin = require('firebase-admin')
+
+// const app = admin.initializeApp()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+
+exports.helpRequest = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+
+ console.log(response)
+//  admin.firestore().doc('help_request/2MAAfEsTGzbAhzoDZ8DP').get().then(data=>{
+//             response.send(data)
+//             // console.log(data)
+//         }).catch(error=>{
+//             response.send(error)
+//         })
+});
